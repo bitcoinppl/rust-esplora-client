abf7de140001c0b319f50dd779876798cd69a2f9
Praveen Perera <me@praveenperera.com>
2024-10-23 12:31:29 -0500

    Don't require `async_trait` in `Sleeper` trait if not async

--- src/sleeper.rs
+++ src/sleeper.rs
@@ -1,8 +1,14 @@
 use std::time::Duration;
 
+#[cfg(feature = "async")]
 #[async_trait::async_trait]
 pub trait Sleeper {
     async fn sleep(duration: Duration);
+}
+
+#[cfg(not(feature = "async"))]
+pub trait Sleeper {
+    fn sleep(duration: Duration);
 }
 
 #[derive(Debug, Clone, Copy)]
